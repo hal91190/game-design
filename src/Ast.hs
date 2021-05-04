138e85226719b24bd2875b091d4681d47ca56a7a
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2021-05-04 20:05:50 +0200

    Revert "mariclou: crée un projet cabal"
    This reverts commit 7ddb2cc6103edf5c216f3fec4239553aad9e4867.

---
+++ src/Ast.hs
@@ -0,0 +1,25 @@
+module Ast where
+
+data Expr
+    = Var String
+    | Integer Int
+    | MTrue | MFalse
+    | List [Expr]
+    | Primitive ([Expr] -> Either String Expr)
+    | Lambda [String] Expr
+    | Apply Expr [Expr]
+    | Let [(String, Expr)] Expr
+    | If Expr Expr Expr
+
+instance Show Expr where
+    show (Integer x) = show x
+    show (List l) = show l
+    show MTrue = "true"
+    show MFalse = "false"
+    show (Var s) = s
+    show (Apply f args) = "(" ++ show f ++ " " ++ show args ++ ")"
+    show (Lambda args e) = "\\" ++ show args ++ "->" ++ show e
+    show (If e1 e2 e3) = "if " ++ show e1 ++ " then " ++ show e2 ++ " else " ++ show e3
+
+data Statement = Statement String [String] Expr
+    deriving (Show)
---
+++ src/Main.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+import Data.Text.IO as IO
+import Text.Megaparsec (parse) 
+import qualified Data.Map as Map
+import Parser (pExpr, pFile)
+import Eval (eval)
+
+main :: IO ()
+main = do
+    code <- IO.readFile "./test.mc"
+    let Right env = parse pFile "./test.mc" code
+    let exprText = "map (\\x -> x * x) [1, 2, 3]"
+    let Right expr = parse pExpr "expr" exprText
+    print $ eval (Map.fromList env) expr
+    --let p2 = eval Map.empty p
+    --print expr
+    --print p2
